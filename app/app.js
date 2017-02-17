032f144e1e475ea3edfd84dbe359acd4a4013ebd
Dylan Nguyen <dnguye92@Yahoo.com>
2017-02-16 18:45:22 -0800

    added an array and a div of ng-repeat

--- app/app.js
+++ app/app.js
@@ -2,12 +2,16 @@ angular.module("todoListApp", [])
 
 .controller('mainCtrl', function($scope) {
   $scope.helloWorld = function() {
-    console.log('Hello There! This is the hello world controller function in the mainCtrl'); 
-  }
-})
-
-.controller('imASibling', function($scope) {
-  $scope.foobar = 1234;
+    console.log("Hello there! This the helloWorld controller function, in the mainCtrl!");
+  };
   
-  //Do other cool stuff
+  $scope.todos = [
+    {"name": "Clean up bruh"},
+    {"name": "Water the dog bruh"},
+    {"name": "Feed the lawn bruh"},
+    {"name": "Pay dem bills bruh"},
+    {"name": "RUN NIGGA"},
+    {"name": "SWIM BRUH"}
+  ]
+
 });
